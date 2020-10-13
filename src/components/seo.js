f5a44164aaa9527a0691635216c197ea0bffaa59
Matt <mroseman95@gmail.com>
2020-10-13 10:10:38 -0400

    added a og:image tag to header that shows a site preview image

--- src/components/seo.js
+++ src/components/seo.js
@@ -52,6 +52,10 @@ function SEO({ description, lang, meta, title }) {
           content: `website`,
         },
         {
+          property: `og:image`,
+          content: `/preview.png`,
+        },
+        {
           name: `twitter:card`,
           content: `summary`,
         },
